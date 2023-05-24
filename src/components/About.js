434bbc82f5a4fb958389caa5e7bb25a0d28ba8e9
unknown <ibr.s.ahmat@gmail.com>
2023-05-24 17:03:18 +0300

    Add linter configurations and fix linter errors

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,13 @@
-const About = () => {
-  return (
-    <div>
-      <div className="about-image1">
-        <img
-          src="/images/car_bg.jpg"
-          alt="car"
-        />
-        <p>ddddddddddddddddddddddddddddddd</p>
-      </div>
+const About = () => (
+  <div>
+    <div className="about-image1">
+      <img
+        src="/images/car_bg.jpg"
+        alt="car"
+      />
+      <p>ddddddddddddddddddddddddddddddd</p>
     </div>
-  );
-};
+  </div>
+);
 
 export default About;
--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,20 +1,18 @@
-const Icon = () => {
-  return (
-    <div className="main_logo_container">
+const Icon = () => (
+  <div className="main_logo_container">
+    <img
+      src="/images/first_bg.jpg"
+      alt="background"
+      className="brightness main_bg"
+    />
+    <div>
       <img
-        src="/images/first_bg.jpg"
-        alt="background"
-        className="brightness main_bg"
+        src="/logo.png"
+        alt="Logo"
+        className="light main_logo"
       />
-      <div>
-        <img
-          src="/logo.png"
-          alt="Logo"
-          className="light main_logo"
-        />
-      </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default Icon;
