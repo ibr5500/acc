019c7e4c7f87a4c924547b33a7dda1f42d25c310
unknown <ibr.s.ahmat@gmail.com>
2023-05-24 03:18:06 +0300

    Build the main loge page

---
+++ src/components/Icon.js
@@ -0,0 +1,20 @@
+const Icon = () => {
+  return (
+    <div className="main_logo_container">
+      <img
+        src="/images/first_bg.jpg"
+        alt="background"
+        className="brightness main_bg"
+      />
+      <div>
+        <img
+          src="/logo.png"
+          alt="Logo"
+          className="light main_logo"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Icon;
