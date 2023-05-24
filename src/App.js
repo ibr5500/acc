a1f52428c218528d22ba346fc997c41946a72b14
unknown <ibr.s.ahmat@gmail.com>
2023-05-24 16:35:17 +0300

    Add about header background image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Icon from './components/Icon';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
       <Icon />
+      <About />
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+const About = () => {
+  return (
+    <div>
+      <div className="about-image1">
+        <img
+          src="/images/car_bg.jpg"
+          alt="car"
+        />
+        <p>ddddddddddddddddddddddddddddddd</p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
