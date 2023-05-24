deb896c2a535e97fa84fa10b83cf8df58f6a86bf
unknown <ibr.s.ahmat@gmail.com>
2023-05-24 20:33:08 +0300

    Fix about us background

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,22 @@ const About = () => (
         src="/images/car_bg.jpg"
         alt="car"
       />
-      <p>ddddddddddddddddddddddddddddddd</p>
+      <div className="about_container">
+        <div className="about_content">
+          <div className="content_1">
+            <h2>من نحن</h2>
+          </div>
+          <div className="content_2">
+            <p>
+              تلتزم شركتنا بتوفير أكثر الوسائل كفاءة وتفوقًا في تأجير أنواع مختلفة من السيارات
+              والمركبات الفاخرة. مع التركيز على الابتكار ، أنشأنا بيئة مميزة وعملية مصممة لتقديم
+              حلول متكاملة تلبي جميع شرائح قاعدة عملائنا المتنوعة. من خلال توفير الوصول إلى أحدث
+              وأرقى المركبات ، نسعى جاهدين لتلبية رغبات واحتياجات كل عميل بأقصى درجات الرضا.
+            </p>
+            <p />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 );
