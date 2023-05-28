197a8f93644dae16d7299ae29fdaba0eda321324
unknown <ibr.s.ahmat@gmail.com>
2023-05-29 01:09:59 +0300

    Fix the goals page layout

--- src/components/Goals.js
+++ src/components/Goals.js
@@ -5,15 +5,15 @@ const Goals = () => (
       <div className="line">..</div>
       <div className="goals-content">
         <p className="desc_en">
-          our company is to revolutionize the world of luxury leasing by fostering an environment of
+          our goal is to revolutionize the world of luxury leasing by fostering an environment of
           creativity and innovation, utilizing obstacles and challenges as opportunities for growth,
           and establishing a collaborative and practical community among all stakeholders in the
           industry. We aim to be the industry leader in performance, setting new standards in luxury
           leasing and elevating the entire sector to new heights
         </p>
         <p className="desc_ar">
-          تهدف شركتنا إلى إحداث ثورة في عالم التأجير الفاخر من خلال تعزيز بيئة من الإبداع والابتكار
-          ، والاستفادة من العقبات والتحديات كفرص للنمو ، وإنشاء مجتمع تعاوني وعملي بين جميع أصحاب
+          هدفنا هو إحداث ثورة في عالم التأجير الفاخر من خلال تعزيز بيئة من الإبداع والابتكار ،
+          والاستفادة من العقبات والتحديات كفرص للنمو ، وإنشاء مجتمع تعاوني وعملي بين جميع أصحاب
           المصلحة في الصناعة. نهدف إلى أن نكون رواد الصناعة في الأداء ، ووضع معايير جديدة في التأجير
           الفاخر ورفع القطاع بأكمله إلى آفاق جديدة
         </p>
