dc49a1f3074ba56e98335f243f777801633f406e
unknown <ibr.s.ahmat@gmail.com>
2023-06-02 00:23:09 +0300

    Fix linter errors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
-import { NavLink } from 'react-router-dom';
-import { FaBars } from 'react-icons/fa';
+// import { NavLink } from 'react-router-dom';
+// import { FaBars } from 'react-icons/fa';
 
 function Navbar() {
   return (
