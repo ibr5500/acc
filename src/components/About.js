52296252413bcd8bb1f4c87eacdae319734f6a78
unknown <ibr.s.ahmat@gmail.com>
2023-06-02 05:18:44 +0300

    fix

--- src/components/About.js
+++ src/components/About.js
@@ -3,12 +3,6 @@ const About = () => (
     id="about"
     className="about_container"
   >
-    <div className="about-image1">
-      <img
-        src="./images/about-img.png"
-        alt="car"
-      />
-    </div>
     <div className="about-items">
       <div className="about_content">
         <div className="content_1">
@@ -48,6 +42,12 @@ const About = () => (
         className="about-arraows-hr"
         src="./Images/arrows-hr.png"
         alt="arrows"
+      />
+    </div>
+    <div className="about-image1">
+      <img
+        src="./images/about-img.png"
+        alt="car"
       />
     </div>
   </div>
