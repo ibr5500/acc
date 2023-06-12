5d37a6f93bf6c69eeab20e2d81d487b3ab017a84
unknown <ibr.s.ahmat@gmail.com>
2023-06-12 22:56:46 +0300

    Fix Contact layout

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,31 +1,133 @@
+import React, { useState } from 'react';
 import Icons from './Icons';
 
-const Contact = () => (
-  <div
-    id="contact"
-    className="contact-contaner"
-  >
-    <div className="contact-content">
-      <div className="contact-items">
-        <div className="contact-header">
-          <h3 className="contact-title">
-            تواصل معنا
-            <div className="line" />
-          </h3>
-        </div>
-        <div className="contact-body">
-          <Icons />
+const Contact = () => {
+  const [isSent, setIsSent] = useState(false);
+  const [data, setData] = useState({
+    fname: '',
+    email: '',
+    message: '',
+  });
+
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    if (data.fname && data.email && data.message) {
+      fetch('https://formspree.io/f/mqknlobw', {
+        method: 'POST',
+        body: JSON.stringify({ data }),
+        headers: { 'Content-Type': 'application/json' },
+      })
+        .then((res) => res.json())
+        .catch((error) => console.log(error)); // eslint-disable-line
+    }
+
+    setTimeout(() => {
+      setData({
+        fname: '',
+        email: '',
+        message: '',
+      });
+      setIsSent((isSent) => !isSent);
+    }, 1500);
+  };
+
+  return (
+    <div
+      id="contact"
+      className="contact-contaner"
+    >
+      <div className="contact-content">
+        <div className="contact-items">
+          <div className="contact-header">
+            <h3 className="contact-title">
+              تواصل معنا
+              <div className="line" />
+            </h3>
+          </div>
+          <div className="contact-details">
+            <form
+              onSubmit={handelSubmit}
+              className=" form gap-3 d-flex flex-column"
+              action="https://formspree.io/f/mqknlobw "
+              method="post"
+            >
+              <div className="col-auto">
+                <label
+                  htmlFor="fname"
+                  className="visually-idden d-flex flex-row"
+                >
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="fname"
+                    name="fname"
+                    placeholder="الاسم الكامل"
+                    maxLength="30"
+                    value={data.fname}
+                    onChange={(e) => setData({ ...data, fname: e.target.value })}
+                    required
+                  />
+                </label>
+              </div>
+              <div className="col-auto">
+                <label
+                  htmlFor="email"
+                  className="visually-idden d-flex flex-row"
+                >
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    name="email"
+                    placeholder="email@gmail.com"
+                    value={data.email}
+                    onChange={(e) => setData({ ...data, email: e.target.value })}
+                    required
+                  />
+                </label>
+              </div>
+              <div className="col-auto">
+                <label
+                  htmlFor="message"
+                  className="visually-idden d-flex flex-row"
+                >
+                  <textarea
+                    type="text"
+                    rows={5}
+                    className="form-control"
+                    id="message"
+                    name="message"
+                    placeholder="اكتب رسالتك هنا ..."
+                    maxLength="350"
+                    value={data.message}
+                    onChange={(e) => setData({ ...data, message: e.target.value })}
+                    required
+                  />
+                </label>
+              </div>
+              <div className="col-auto">
+                <button
+                  type="submit"
+                  className={isSent ? 'mail-btn fly' : 'mail-btn'}
+                >
+                  Send
+                </button>
+              </div>
+            </form>
+            <div className="contact-body">
+              <Icons />
+            </div>
+          </div>
         </div>
       </div>
+      <foot>
+        <img
+          src="./assets/contact-bg.WebP"
+          alt="contact_img"
+          className="contact-img-bg"
+        />
+      </foot>
     </div>
-    <foot>
-      <img
-        src="./assets/contact-bg.WebP"
-        alt="contact_img"
-        className="contact-img-bg"
-      />
-    </foot>
-  </div>
-);
-
+  );
+};
 export default Contact;
--- src/components/Icons.js
+++ src/components/Icons.js
@@ -1,9 +1,19 @@
 import { FaMapMarkerAlt } from 'react-icons/fa';
-import { MdEmail } from 'react-icons/md';
+// import { MdEmail } from 'react-icons/md';
 import { IoLogoWhatsapp } from 'react-icons/io';
 
 const Icons = () => (
   <div className="icons-container">
+    <div className="icon map">
+      <a
+        href="https://goo.gl/maps/oQTvNrrpxDSM5us99"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FaMapMarkerAlt className="icon-logo map-logo" />
+        <span>المملكة العربية السعودية, جدة, حي المحمدية, شارع زيد الأنصاري</span>
+      </a>
+    </div>
     <div className="icon whatsapp">
       <a
         aria-label="Chat on WhatsApp"
@@ -14,27 +24,7 @@ const Icons = () => (
         <IoLogoWhatsapp className="icon-logo whatsapp-logo" />
         <span>+966593084741</span>
       </a>
-    </div>
-    <div className="icon email">
-      <a
-        aria-label="Send an Email"
-        href="mailto:info@tasaraa.com"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <MdEmail className="icon-logo email-logo" />
-        <span style={{ fontFamily: 'sans-serif' }}>info@tasaraa.com</span>
-      </a>
-    </div>
-    <div className="icon map">
-      <a
-        href="https://goo.gl/maps/oQTvNrrpxDSM5us99"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <FaMapMarkerAlt className="icon-logo map-logo" />
-        <span>جدة, حي المحمدية, شارع زيد الأنصاري</span>
-      </a>
+      - للتواصل عبر الواتساب
     </div>
   </div>
 );
